4a7012fdd98e59eb37aea616214e8d013755ae71
lihanglucien <lihanglucien@didichuxing.com>
2017-10-18 17:35:57 +0800

    update

---
+++ ngx_on_cpu_lua.sh
@@ -0,0 +1,13 @@
+#bin/bash
+
+path=ngx_on_cpu_lua
+
+mkdir $path
+
+openresty-systemtap-toolkit/ngx-sample-lua-bt -p $1 -t 5 --luajit20 -t 5 > $path/tmp.bt
+
+openresty-systemtap-toolkit/fix-lua-bt $path/tmp.bt > $path/$.bt
+
+
+FlameGraph/stackcollapse-stap.pl $path/$1_new.bt > $path/$1.cbt
+FlameGraph/flamegraph.pl  --title="On CPU Leak Flame Graph"  $path/$1.cbt > $path/$1.svg
